f0baa675c4df8b159b5dcbce49dca63f3b1b8ab9
Bhavy Khatri <bkhatri@microsoft.com>
2021-06-12 14:05:44 +0530

    wiki changes working

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: 'src/manifest.json',
+            to: path.join(__dirname, 'dist'),
+            force: true,
+            transform: function (content, path) {
+              // generates the manifest file using the package.json informations
+              return Buffer.from(
+                JSON.stringify({
+                  description: process.env.npm_package_description,
+                  version: process.env.npm_package_version,
+                  ...JSON.parse(content.toString()),
+                })
+              );
+            },
+          },
+        ],
+      }),
+    ]
+};
